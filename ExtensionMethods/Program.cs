d41e3102293fda3ecb25f5f13485b7f967a3eaff
nirzaf <mfmfazrin1986@gmail.com>
2022-01-28 09:14:33 +0530

    global using static System.Console;

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -12,12 +12,9 @@ namespace ExtensionMethods
         {
             string post = "This is supposed to be a very long blog post blah blah blah...";
             var shortenedPost = post.Shorten(5);
-
             IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18 };
             var max = numbers.Max();
-
             Console.WriteLine(max);
-
         }
     }
 }
