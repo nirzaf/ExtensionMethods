ef228f4d8a36262f873a45d6c2a7a5968209d882
nirzaf <mfmfazrin1986@gmail.com>
2022-01-28 09:10:58 +0530

    Add project files.

---
+++ ExtensionMethods/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ExtensionMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a very long blog post blah blah blah...";
+            var shortenedPost = post.Shorten(5);
+
+            IEnumerable<int> numbers = new List<int>() { 1, 5, 3, 10, 2, 18 };
+            var max = numbers.Max();
+
+            Console.WriteLine(max);
+
+        }
+    }
+}
---
+++ ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static string Shorten(this String str, int numberOfWords)
+        {
+            if (numberOfWords < 0)
+                throw new ArgumentOutOfRangeException("numberOfWords should be greater than or equal to 0.");
+
+            if (numberOfWords == 0)
+                return "";
+
+            var words = str.Split(' ');
+
+            if (words.Length <= numberOfWords)
+                return str;
+
+            return string.Join(" ", words.Take(numberOfWords)) + "...";
+        }
+    }
+}
